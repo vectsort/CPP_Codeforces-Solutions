80f41a2047b917611fa137e46fee187c741abd5e
vectsort <jason@haryan.to>
2026-02-20 19:56:55 +0700

    1. 4A.cpp

---
+++ 1. 4A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+//template jason bos
+#define ll long long
+#define pb push_back
+#define pass void(0)
+#define fastio ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr)
+#define all(x) x.begin(),x.end()
+using namespace std;
+ 
+int main() 
+{
+  int n;
+  cin>>n;
+  if (n%2==0&&n>2){
+    cout<<"YES\n";
+  }else{
+    cout<<"NO\n";
+  }
+}
